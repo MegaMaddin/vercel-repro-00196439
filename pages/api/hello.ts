0e5cd02ffe9549847f21f2b3d46750c32bda4c38
Martin Mazein <internet+github@amazeing.net>
2024-07-05 16:49:18 +0200

    WIP
    Signed-off-by: Martin Mazein <internet+github@amazeing.net>

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { parse } from "json2csv";
 
 type Data = {
-  name: string;
+  csv: any;
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const data = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: `Bret's`,
+    },
+    {
+      id: 2,
+
+      name: "Ervin Howell",
+      username: "Antonette's",
+    },
+  ];
+
+  console.log(data);
+  console.log(JSON.stringify(data, null, 2));
+
+  const csv = parse(data);
+
+  console.log(csv);
+  console.log(JSON.stringify(csv, null, 2));
+
+  res.status(200).json({ csv });
 }
